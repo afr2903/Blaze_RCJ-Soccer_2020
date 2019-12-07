92c574ef5fa7e1cc6965ee41da255aa329c8f636
afr2903 <afr102903@gmail.com>
2019-12-06 19:17:38 -0600

    Libreria update

--- Teensy/libraries/Omni/Omni.cpp
+++ Teensy/libraries/Omni/Omni.cpp
@@ -1,47 +1,53 @@
 #include "Arduino.h"
 #include "Omni.h"
+//4--1
+//|  |
+//3--2
 
-Omni::Omni(int _pw[], int _dr[], int _di[], float _kp){
-    for(int i=1; i<=4; i++){
-        pw[i]=_pw[i];
-        dr[i]=_dr[i];
-        di[i]=_di[i];
-        pinMode(pw[i],OUTPUT);
-        pinMode(dr[i],OUTPUT);
-        pinMode(di[i],OUTPUT);
+Omni::Omni(int _n_mot, int _pw[], int _dr[], int _di[], float _kp, float _kd){ //Constructor
+    n_mot= _n_mot;
+    for(int i=1; i<=n_mot; i++){
+        pinMode(pw[i]=_pw[i] ,OUTPUT);
+        pinMode(dr[i]=_dr[i] ,OUTPUT);
+        pinMode(di[i]=_di[i] ,OUTPUT);
+        mot_ang[i]= (360/n_mot)*(i-.5);
     }
-    kp= _kp;
+    kp= _kp; //Constantes del control
+    kd= _kd;
 }
 
-void Omni::motor(int n, int sp, bool dir){
-    n= n-'A'+1;
+void Omni::motor(int n, float sp){ //Movimiento de un motor individual. n= numero del motor / sp= velocidad (puede ser negativa)
+    bool dir= sp>0? 1:0; //Direccion del motor (1 es derecha y 0 es izquierda)
+    sp= min(255, max(0,abs(sp))); //Normalizar velocidad entre 0 y 255
     analogWrite(pw[n], sp);
-    digitalWrite(dr[n], dir);
+    digitalWrite(dr[n], dir);   //Mover motor
     digitalWrite(di[n], !dir);
 }
 void Omni::test(){
-    for(int i='A'; i<='D'; i++){
-        motor(i, 255, 1);
-        delay(1000);
-        motor(i, 255, 0);
-        delay(1000);
-        motor(i, 0, 0);
+    for(int i=1; i<=n_mot; i++){
+        motor(i, 255); //Derecha
+        delay(500);
+        motor(i, -255); //Izquierda
+        delay(500);
+        motor(i, 0); //Apagar
     }
 }
-void Omni::align(float error){
-    error>0? aux=0:aux=1;
-    error= abs(error);
-    for(int i='A'; i<='D'; i++)
-        motor(i, error*kp, aux);
+void Omni::align(float error){ //Alinearse sobre su propio eje. error= valor del sensor de orientación
+    pid= (error*kp)+(error-last_error)*kd; //Control proporcional y derivativo
+    last_error= error;
+    for(int i=1; i<=n_mot; i++) //Mover cada uno de los motores
+        motor(i, -pid);
 }
-void Omni::move(int deg, int sp, float error){
-    bool dir;
-    for(int i='A'; i<='D'; i++){
-        float mot_sp= sin((deg-ang[i-'A'+1])*PI/180)*sp;
-        dir= mot_sp>0? 1:0;
-        mot_sp= abs(mot_sp);
-        mot_sp+= dir? error*kp:-error*kp;
+void Omni::move(int deg, int sp, float error){ //Movimientos omnidireccionales. deg= angulo de direccion del robot(-360 a 360) / sp= velocidad de los motores / error= valor del sensor de orientación
+    pid= (error*kp)+(error-last_error)*kd; //Control proporcional y derivativo
+    last_error= error;
+    float mot_sp[n_mot+1], max_sp=0;
+    for(int i=1; i<=n_mot; i++)
+        max_sp= max(abs(mot_sp[i]= sin((deg-mot_ang[i])*PI/180)),max_sp); //Maximizar velocidad a 1
 
-        motor(i, mot_sp, dir);
+    float scale= 1/max_sp;
+    for(int i=1; i<=n_mot; i++){
+        mot_sp[i]= mot_sp[i]*scale*sp - pid; //Obtener velocidad individual del motor para moverse en la direccion deseada con control
+        motor(i, mot_sp[i]);
     }
 }
--- Teensy/libraries/Omni/Omni.h
+++ Teensy/libraries/Omni/Omni.h
@@ -2,20 +2,24 @@
 #define Omni_h
 
 #include "Arduino.h"
+#define MAX 10
+//4--1
+//|  |
+//3--2
 
 class Omni
 {
   public:
-    Omni(int _pw[], int _dr[], int _di[], float _kp);
-    void motor(int n, int sp, bool dir);
-    void test();
-    void align(float error);
-    void move(int deg, int sp, float error);
+    Omni(int _n_mot, int _pw[], int _dr[], int _di[], float _kp, float _kd); //Constructor
+    void motor(int n, float sp); //Movimiento de un motor individual
+    void test(); //Probar motores uno por uno
+    void align(float error); //Alinearse sobre su propio eje
+    void move(int deg, int sp, float error); //Movimientos omnidireccionales
   private:
-    int pw[5], dr[5], di[5];
-    int ang[5]={0,45,135,225,315};
-    bool aux;
-    float kp;
+    int n_mot;
+    int pw[MAX], dr[MAX], di[MAX]; //Pines de los motores
+    int mot_ang[MAX]; //Angulo en que se encuentran posicionados los motores
+    float kp, kd, last_error, pid; //Variables para el control
 };
 
 #endif
