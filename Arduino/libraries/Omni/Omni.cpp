2f3aad8e481692a8aadc88892412ea8338558d19
afr2903 <afr102903@gmail.com>
2019-12-31 03:19:59 -0600

    Motor & Control library 2.0
    Pointer update

--- Arduino/libraries/Omni/Omni.cpp
+++ Arduino/libraries/Omni/Omni.cpp
@@ -1,53 +1,65 @@
 #include "Arduino.h"
 #include "Omni.h"
-//4--1
-//|  |
-//3--2
 
-Omni::Omni(int _n_mot, int _pw[], int _dr[], int _di[], float _kp, float _kd){ //Constructor
-    n_mot= _n_mot;
-    for(int i=1; i<=n_mot; i++){
-        pinMode(pw[i]=_pw[i] ,OUTPUT);
-        pinMode(dr[i]=_dr[i] ,OUTPUT);
-        pinMode(di[i]=_di[i] ,OUTPUT);
-        mot_ang[i]= (360/n_mot)*(i-.5);
-    }
-    kp= _kp; //Constantes del control
-    kd= _kd;
+//CONSTRUCTORES
+Omni::Omni(int _nMot, motor **_mot){
+    nMot= _nMot;
+    mot= _mot;
+    for(int i=0; i<nMot; i++)
+        (*mot[i]).angle= (360/nMot)*(i+.5);
+}
+motor::motor(int rP, int lP, int pP){
+    if(pP) pinMode(pwmPin=pP, OUTPUT);
+    pinMode(rightPin=rP, OUTPUT);
+    pinMode(leftPin=lP, OUTPUT);
 }
 
-void Omni::motor(int n, float sp){ //Movimiento de un motor individual. n= numero del motor / sp= velocidad (puede ser negativa)
-    bool dir= sp>0? 1:0; //Direccion del motor (1 es derecha y 0 es izquierda)
-    sp= min(255, max(0,abs(sp))); //Normalizar velocidad entre 0 y 255
-    analogWrite(pw[n], sp);
-    digitalWrite(dr[n], dir);   //Mover motor
-    digitalWrite(di[n], !dir);
+//MOTOR CONTROL
+void motor::moveMotor(int speed){
+    bool dir= speed>0? 1:0;
+    sp= min(255, max(0,abs(speed)));
+    if(pwmPin) analogWrite(pwmPin, speed);
+    digitalWrite(rightPin, dir);
+    digitalWrite(leftPin, !dir);
 }
-void Omni::test(){
-    for(int i=1; i<=n_mot; i++){
-        motor(i, 255); //Derecha
+double motor::omniSpeed(int target, int speed){
+    return sp= sin((target-angle)*PI/180)*speed;
+}
+
+//PID CONTROL
+void Omni::setPID(double _kp, double _ki, double _kd){
+    kp=_kp, ki=_ki, kd=_kd;
+    integral= lastError= 0;
+}
+double Omni::getPID(double error){
+    derivative= error-lastError;
+    integral+= error;
+    lastError= error;
+    return (error*kp)+(integral*ki)+(derivative*kd);
+}
+
+//MOVEMENT FUNCTIONS
+void Omni::test(int speed){
+    for(int i=0; i<nMot; i++){
+        (*mot[i]).moveMotor(speed);
         delay(500);
-        motor(i, -255); //Izquierda
+        (*mot[i]).moveMotor(-speed);
         delay(500);
-        motor(i, 0); //Apagar
+        (*mot[i]).moveMotor(0);
     }
 }
-void Omni::align(float error){ //Alinearse sobre su propio eje. error= valor del sensor de orientación
-    pid= (error*kp)+(error-last_error)*kd; //Control proporcional y derivativo
-    last_error= error;
-    for(int i=1; i<=n_mot; i++) //Mover cada uno de los motores
-        motor(i, -pid);
+void Omni::align(double error){
+    double pid= getPID(error);
+    for(int i=0; i<nMot; i++)
+        (*mot[i]).moveMotor(-pid);
 }
-void Omni::move(int deg, int sp, float error){ //Movimientos omnidireccionales. deg= angulo de direccion del robot(-360 a 360) / sp= velocidad de los motores / error= valor del sensor de orientación
-    pid= (error*kp)+(error-last_error)*kd; //Control proporcional y derivativo
-    last_error= error;
-    float mot_sp[n_mot+1], max_sp=0;
-    for(int i=1; i<=n_mot; i++)
-        max_sp= max(abs(mot_sp[i]= sin((deg-mot_ang[i])*PI/180)),max_sp); //Maximizar velocidad a 1
+void Omni::move(int target, int speed, double error){
+    double pid= getPID(error);
+    double maxSp=0;
+    for(int i=0; i<nMot; i++)
+        maxSp= max( abs((*mot[i]).omniSpeed(target,speed)), maxSp);
 
-    float scale= 1/max_sp;
-    for(int i=1; i<=n_mot; i++){
-        mot_sp[i]= mot_sp[i]*scale*sp - pid; //Obtener velocidad individual del motor para moverse en la direccion deseada con control
-        motor(i, mot_sp[i]);
-    }
+    double scale= speed/maxSp;
+    for(int i=0; i<nMot; i++)
+        (*mot[i]).moveMotor( (*mot[i]).sp*scale - pid );
 }
--- Arduino/libraries/Omni/Omni.h
+++ Arduino/libraries/Omni/Omni.h
@@ -2,24 +2,31 @@
 #define Omni_h
 
 #include "Arduino.h"
-#define MAX 10
-//4--1
-//|  |
-//3--2
+
+struct motor{
+    int rightPin;
+    int leftPin;
+    int pwmPin=0;
+    int angle=0;
+    double sp=0;
+    motor(int rP, int lP, int pP=0);
+    void moveMotor(int speed);
+    double omniSpeed(int target, int speed);
+};
 
 class Omni
 {
   public:
-    Omni(int _n_mot, int _pw[], int _dr[], int _di[], float _kp, float _kd); //Constructor
-    void motor(int n, float sp); //Movimiento de un motor individual
-    void test(); //Probar motores uno por uno
-    void align(float error); //Alinearse sobre su propio eje
-    void move(int deg, int sp, float error); //Movimientos omnidireccionales
+    Omni(int _nMot, motor **_mot);
+    void test(int speed);
+    void setPID(double _kp, double _ki, double _kd);
+    double getPID(double error);
+    void align(double error);
+    void move(int target, int speed, double error);
   private:
-    int n_mot;
-    int pw[MAX], dr[MAX], di[MAX]; //Pines de los motores
-    int mot_ang[MAX]; //Angulo en que se encuentran posicionados los motores
-    float kp, kd, last_error, pid; //Variables para el control
+    int nMot;
+    double kp, ki, kd, integral, derivative, lastError;
+    motor **mot;
 };
 
 #endif
