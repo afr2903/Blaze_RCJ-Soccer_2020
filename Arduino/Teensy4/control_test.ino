245f704426fb6223b34b4e8acb04f6cc9abaae30
afr2903 <afr102903@gmail.com>
2019-12-31 03:56:28 -0600

    Control PD quaternion tests

--- Arduino/Teensy4/control_test.ino
+++ Arduino/Teensy4/control_test.ino
@@ -5,15 +5,16 @@
 #include <utility/imumaths.h>
   
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
-sensors_event_t event; 
 
-int pw[]={0,22,14,4,5};
-int dr[]={0,20,15,3,7};
-int di[]={0,21,16,2,6};
-int dir;
-float deg;
+motor m1(21,22,3);
+motor m2(5,4,6);
+motor m3(7,8,9);
+motor m4(11,10,12);
+motor *motors[]= {&m1,&m2,&m3,&m4};
+Omni cbaq(4, motors);
 
-Omni cbaq(pw,dr,di,1.0,0.5);
+double kp=3.3, ki=0, kd=80;
+double target, error, strength;
 
 void setup() {
   Serial.begin(19200);
@@ -22,31 +23,23 @@ void setup() {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
+  Serial.println("Calibrating...");
+  uint8_t sys, gyro, accel, mag;
+  sys = gyro = accel = mag = 0;
+  while (mag != 3)
+    bno.getCalibration(&sys, &gyro, &accel, &mag);
+  Serial.println(mag, DEC);
   delay(500);
-    
   bno.setExtCrystalUse(true);
-  
+
+  cbaq.setPID(kp,ki,kd);
 }
 
 void loop(){
   
-  if (Serial1.available() > 0) {
-    dir = Serial1.read()-51;
-    dir+= dir>5? +5: dir<5? -5:0;
-    Serial.println(dir);
-    //delay(500);
-  }
-  //else cbaq.move(dir, 0, deg);
+  error= bno.getError();
+  Serial.println(error);
   
-  bno.getEvent(&event);
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  deg= euler.x();
-  if(deg>180) deg-=360;
-  //Serial.println(deg);
-  
-  cbaq.move(dir, 150, deg);
-  //cbaq.align(deg);
-  //cbaq.motor('D', 60, 1);
-  //cbaq.test();
-  //delay(50);
+  cbaq.move(0-error, 200, error);
+  //cbaq.align(error);
 }
