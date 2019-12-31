3607f51554e493e0c49b75dbce960a138c9f99bc
afr2903 <afr102903@gmail.com>
2019-12-31 03:38:19 -0600

    Motor test w updated library

--- Arduino/Teensy4/motor_test.ino
+++ Arduino/Teensy4/motor_test.ino
@@ -1,16 +1,19 @@
 #include <Omni.h>
-int pw[]={0,22,14,4,5};
-int dr[]={0,20,15,3,7};
-int di[]={0,21,16,2,6};
 
-Omni cbaq(pw,dr,di,1.0,1.0);
+motor m1(21,22,3);
+motor m2(5,4,6);
+motor m3(7,8,9);
+motor m4(11,10,12);
+motor *motors[]= {&m1,&m2,&m3,&m4};
+double kp=3.3, ki=0, kd=80;
+Omni cbaq(4, motors);
 
 void setup() {
-  
+  cbaq.setPID(kp,ki,kd);
 }
 
 void loop(){
   //cbaq.move(90, 100, 0);
   //cbaq.motor('D', 60, 1);
-  cbaq.test();
+  cbaq.test(100);
 }
