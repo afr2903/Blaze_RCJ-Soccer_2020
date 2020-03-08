be34ce88d867f9e8c7c41395896d0185eaec96e2
afr2903 <afr102903@gmail.com>
2020-03-07 20:43:01 -0600

    Motor testing Ness

--- Arduino/Teensy4/motor_test.ino
+++ Arduino/Teensy4/motor_test.ino
@@ -1,19 +1,21 @@
-#include <Omni.h>
+#include <Drive.h>
 
-motor m1(21,22,3);
-motor m2(5,4,6);
-motor m3(7,8,9);
-motor m4(11,10,12);
+motor m1(2,4,3);
+motor m2(23,21,22);
+motor m3(10,8,9);
+motor m4(7,5,6);
 motor *motors[]= {&m1,&m2,&m3,&m4};
 double kp=3.3, ki=0, kd=80;
-Omni cbaq(4, motors);
+Drive ness(4, motors);
 
 void setup() {
-  cbaq.setPID(kp,ki,kd);
+  Serial.begin(19200);
+  ness.setPID(kp,ki,kd);
 }
 
 void loop(){
-  //cbaq.move(90, 100, 0);
-  //cbaq.motor('D', 60, 1);
-  cbaq.test(100);
+  ness.test(0.5);
+  //m2.moveMotor(1.0);
+  //Serial.println(digitalRead(12));
+  //delay(100);
 }
