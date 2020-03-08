a5774cbbb75e9998aa559b89729614999f24c28e
afr2903 <afr102903@gmail.com>
2020-03-07 20:44:47 -0600

    Ball follower update

--- Arduino/Teensy4/vision_test.ino
+++ Arduino/Teensy4/vision_test.ino
@@ -1,4 +1,4 @@
-#include <Omni.h>
+#include <Drive.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
@@ -6,12 +6,12 @@
   
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-motor m1(21,22,3);
-motor m2(5,4,6);
-motor m3(7,8,9);
-motor m4(11,10,12);
+motor m1(2,4,3);
+motor m2(23,21,22);
+motor m3(10,8,9);
+motor m4(7,5,6);
 motor *motors[]= {&m1,&m2,&m3,&m4};
-Omni cbaq(4, motors);
+Drive ness(4, motors);
 
 double kp=3.3, ki=0, kd=80;
 double target, error, strength;
@@ -23,33 +23,34 @@ void setup() {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  Serial.println("Calibrating...");
+  /*Serial.println("Calibrating...");
   uint8_t sys, gyro, accel, mag;
   sys = gyro = accel = mag = 0;
   while (mag != 3) {
     bno.getCalibration(&sys, &gyro, &accel, &mag);
   }
   Serial.println(mag, DEC);
-  delay(500);
+  delay(500);*/
   bno.setExtCrystalUse(true);
 
-  cbaq.setPID(kp,ki,kd);
+  ness.setPID(kp,ki,kd);
 }
 
 void loop(){
   
   if (Serial1.available() > 0) {
-    target = Serial1.read()-100;
-    strength= Serial1.read();
+    target = Serial1.read()-120;
+    strength= Serial1.read()*1.5;
   }
 
-  Serial.print(target);
+  //strength = strength/255;
+  /*Serial.print(target);
   Serial.print(" ");
   Serial.print(strength);
-  Serial.print(" ");
+  Serial.print(" ");*/
   
   error= bno.getError();
-  Serial.println(error);
+  //Serial.println(error);
   
-  cbaq.move(target-error, 200, error);
+  ness.move(target-error, 100+strength, error);
 }
