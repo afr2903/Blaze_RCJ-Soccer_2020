592ff78a55108ea494334260d06becffda11407a
afr2903 <afr102903@gmail.com>
2020-03-07 20:43:40 -0600

    Align robot test

--- Arduino/Teensy4/control_test.ino
+++ Arduino/Teensy4/control_test.ino
@@ -1,4 +1,4 @@
-#include <Omni.h>
+#include <Drive.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
@@ -6,14 +6,14 @@
   
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-motor m1(21,22,3);
-motor m2(5,4,6);
-motor m3(7,8,9);
-motor m4(11,10,12);
+motor m1(2,4,3);
+motor m2(23,21,22);
+motor m3(10,8,9);
+motor m4(7,5,6);
 motor *motors[]= {&m1,&m2,&m3,&m4};
-Omni cbaq(4, motors);
-
 double kp=3.3, ki=0, kd=80;
+
+Drive ness(4, motors);
 double target, error, strength;
 
 void setup() {
@@ -23,23 +23,23 @@ void setup() {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  Serial.println("Calibrating...");
+  /*Serial.println("Calibrating...");
   uint8_t sys, gyro, accel, mag;
   sys = gyro = accel = mag = 0;
   while (mag != 3)
     bno.getCalibration(&sys, &gyro, &accel, &mag);
   Serial.println(mag, DEC);
-  delay(500);
+  delay(500);*/
   bno.setExtCrystalUse(true);
 
-  cbaq.setPID(kp,ki,kd);
+  ness.setPID(kp,ki,kd);
 }
 
 void loop(){
   
   error= bno.getError();
-  Serial.println(error);
+  //Serial.println(error);
   
-  cbaq.move(0-error, 200, error);
-  //cbaq.align(error);
+  ness.move(0-error, 0.7, error);
+  //ness.align(error);
 }
